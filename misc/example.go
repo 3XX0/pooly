08e89a4d37eb2ab85056e60f6b80fa32d8a4f4f6
Jonathan Calmels <jbjcalmels@gmail.com>
2014-07-03 23:18:24 +0200

    Update example.go

--- misc/example.go
+++ misc/example.go
@@ -4,7 +4,10 @@ import "github.com/3XX0/pooly"
 
 func main() {
 	s := pooly.NewService("netcat", nil)
+	defer s.Close()
+
 	s.Add("127.0.0.1:1234")
+	s.Add("127.0.0.1:12345")
 
 	c, err := s.GetConn()
 	if err != nil {
@@ -12,12 +15,23 @@ func main() {
 		return
 	}
 
-	_, err = c.NetConn().Write([]byte("ping"))
+	_, err = c.NetConn().Write([]byte("ping\n"))
 
-	if err := c.Release(err, 1); err != nil {
+	if err := c.Release(err, pooly.HostUp); err != nil {
 		println("could not release connection")
 		return
 	}
 
-	s.Close()
+	c, err = s.GetConn()
+	if err != nil {
+		println("could not get connection")
+		return
+	}
+
+	_, err = c.NetConn().Write([]byte("ping\n"))
+
+	if err := c.Release(err, pooly.HostUp); err != nil {
+		println("could not release connection")
+		return
+	}
 }
